f82cd2e3a8918900c500dbb70f71d086b75431a9
Emad Idris <emadidris@outlook.com>
2021-12-06 14:53:49 +0200

    lab27

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 
 
 function Footer(){
-  return (<footer>&copy; 2018</footer>);
+  return (<footer>&copy; 2021 --- Emad Idris</footer>);
 }
-// class Footer extends React.Component {
-//   render() {
-//     return <footer>&copy; 2018</footer>;
-//   }
-// }
-
 export default Footer;
